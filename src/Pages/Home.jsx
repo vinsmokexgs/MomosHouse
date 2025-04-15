93648c49c7f25117776db7ac39783649605d1134
Anu Kawan <anukawan268@gmail.com>
2025-04-15 10:57:48 +0545

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,6 +10,12 @@ import NavigationToMenu from "../Components/NavigationToMenu";
 import Quality from "../assets/Home/Quality.png";
 import Chef from "../assets/Home/Chef.png";
 import Party from "../assets/Home/party.png";
+import { FaMapMarkerAlt } from "react-icons/fa";
+import { FaPhone } from "react-icons/fa";
+import { FaClock } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
 
 function Home() {
   const [recipes, setrecipes] = useState([]);
@@ -404,7 +410,7 @@ function Home() {
           </div>
         </div>
 
-        
+
         <div>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3533.481059204379!2d85.34317037525248!3d27.67152297620314!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb190536c1caa7%3A0xf92fcf603dac3960!2sSipalaya%20Info%20Tech%20Pvt.%20Ltd.!5e0!3m2!1sen!2snp!4v1743051090798!5m2!1sen!2snp"

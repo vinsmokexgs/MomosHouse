44d065c9d11acfe776582eb1847213bef6d75b8d
Anu Kawan <anukawan268@gmail.com>
2025-04-16 16:11:16 +0545

    party

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -49,7 +49,7 @@ const Services = () => {
 
       <div className='flex items-center pl-10  ml-50 mr-20 mt-10 gap-40'>
         <div className="h-[20%]  flex items-center justify-end">
-          <img src={Party} />
+          <img src={party} />
         </div>
         <div className=''>
           <img src={Chef} />
